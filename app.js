c7670f069a263da351313d0217cb3320cd1878b7
Stalin Pilapanta <pilapantastalin@gmail.com>
2020-03-18 13:20:00 -0500

    Strings

---
+++ app.js
@@ -0,0 +1,26 @@
+var nombre, mensaje;
+
+mensaje = 'Y entonces dije \'Buen Curso!!\'';
+
+
+console.log(mensaje);
+
+let aprendiendo = 'aprendiendo',
+    tecnologia = 'JavaScript';
+
+console.log(aprendiendo.length);
+console.log(aprendiendo.concat(' HOla'))
+console.log(aprendiendo.toUpperCase());
+
+let texto = 'Aprendiendo javascript, html, css para ser FrontEnd';
+console.log(texto.substring(12, 22))
+
+console.log(texto.slice(0, -11))
+
+console.log(texto.split(','))
+
+console.log(texto.replace('css', 'react'))
+
+console.log(texto.includes('css'))
+
+console.log(texto.repeat(10))
