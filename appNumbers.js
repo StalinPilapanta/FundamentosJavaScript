76fad1bab2ca466852ae8e5f5b5b1d2c37cf2783
Stalin Pilapanta <pilapantastalin@gmail.com>
2020-03-18 13:40:39 -0500

    Video 15

--- appNumbers.js
+++ appNumbers.js
@@ -16,4 +16,36 @@ resultado = numero1 / numero2;
 
 resultado = numero1 % numero2;
 
-console.log(resultado)
+
+resultado = Math.PI;
+
+resultado = Math.round(3.5);
+
+resultado = Math.ceil(2.1);
+
+resultado = Math.floor(2.1);
+
+resultado = Math.sqrt(144)
+
+resultado = Math.abs(numero5);
+
+
+resultado = Math.pow(8, 3);
+
+resultado = Math.min(3, 5, 7, 8);
+
+resultado = Math.max(3, 5, 7, 8)
+
+resultado = Math.random() * 10;
+
+
+resultado = (10 + 20) * 5;
+
+resultado = (10 + 20 + 20 + 10 + 40) * .20;
+
+let puntaje = 10;
+
+
+
+
+console.log(puntaje)
