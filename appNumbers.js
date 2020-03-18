8e3898fcf084ac94528942f611b0c9bd593311e2
Stalin Pilapanta <pilapantastalin@gmail.com>
2020-03-18 13:25:29 -0500

    15  numeros en javascript

---
+++ appNumbers.js
@@ -0,0 +1,19 @@
+const numero1 = 30,
+    numero2 = 20,
+    numero3 = 20.20,
+    numero4 = -1020,
+    numero5 = -3
+
+let resultado;
+
+resultado = numero1 + numero2
+
+resultado = numero1 - numero2
+
+resultado = numero1 * 2;
+
+resultado = numero1 / numero2;
+
+resultado = numero1 % numero2;
+
+console.log(resultado)
