3008be31e5635c682fd7bd88000a01246f921347
Stalin Pilapanta <pilapantastalin@gmail.com>
2020-03-23 20:53:17 -0500

    try catch

--- appNumbers.js
+++ appNumbers.js
@@ -1,19 +1,14 @@
-    const musica = {
-        reproducir: function(id) {
-            console.log(`Reproduciendo musica con id: ${id}`)
-        },
-        pause: function(id) {
-            console.log(`Pausando la musica con id: ${id}`)
-        }
-    }
+obtenerDatos();
 
+try {
+    datos()
+} catch (error) {
+    console.log(error);
+}
 
-    musica.reproducir(2);
-
-    musica.pause(3);
-
-    musica.borrar = function(id) {
-        console.log(`Borrando la musica con id: ${id}`)
-    }
-
-    musica.borrar(2);
+function obtenerDatos() {
+    console.log('Descargando...');
+    setTimeout(function() {
+        console.log('Completado.');
+    }, 3000);
+}
