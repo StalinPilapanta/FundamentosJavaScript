cc2ab499f6335b0d96dc097e358f11c7fd30a762
Stalin Pilapanta <pilapantastalin@gmail.com>
2021-09-09 14:43:51 -0500

    Ejercicio Objetos JS

--- 1ConceptosBasicos/appNumbers.js
+++
@@ -1,8 +0,0 @@
-const pendientes = ['Comer', 'Tarea', 'Aprender JavaScript'];
-
-
-pendientes.forEach(function(pendientes, index) {
-    console.log(`Aprender ${index} ${pendientes}`);
-})
-
-console.log(pendientes);
---
+++ pruebas.js
@@ -0,0 +1,38 @@
+function autos(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;
+
+}
+
+let i = 4
+
+for (let e = 0; i < i; e++) {
+    let valor = prompt("Ingrese la cantidad")
+    console.log()
+}
+
+function ingresocoche() {
+
+    let cantidad = document.getElementById('cantidadCoches').value
+    let marca = document.getElementById("marca").value
+    let modelo = document.getElementById("modelo").value
+    let annio = document.getElementById("annio").value
+
+    for (let i = 0; i <= cantidad; i++) {
+
+        let valor = prompt("Ingrese la cantidad")
+        let auto = new autos()
+        auto.marca = marca
+        auto.modelo = modelo
+        auto.annio = annio
+
+        let pintaMarca = document.getElementById("marcaCoche")
+        pintaMarca.innerText = marca
+        let pintaModelo = document.getElementById("modeloCoche")
+        pintaMarca.innerText = modelo
+        let pintaAnnio = document.getElementById("annioCoche")
+        pintaAnnio.innerText = annio
+    }
+
+}
